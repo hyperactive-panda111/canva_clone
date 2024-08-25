ee99a3211e04284e72d57bb55100775a6927b03d
hyperactive-panda111 <111157902+hyperactive-panda111@users.noreply.github.com>
2024-08-25 20:50:17 +0000

    Update fill-color-sideabar.tsx
    added getActiveFillColor function to get fill color of last selected shape.

--- image-ai/src/features/editor/components/fill-color-sideabar.tsx
+++ image-ai/src/features/editor/components/fill-color-sideabar.tsx
@@ -16,7 +16,7 @@ export const FillColorSidebar = ({
     activeTool,
     onChangeActiveTool,
 }: FillColorSidebarProps) => {
-    const value = editor?.fillColor || FILL_COLOR;
+    const value = editor?.getActiveFillColor() || FILL_COLOR;
     
     const onClose = () => {
         onChangeActiveTool('select');
