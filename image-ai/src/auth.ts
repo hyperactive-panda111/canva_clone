9f0e5219d9722681ae2aa2d1705626c55e71576b
amomensah675 <amomensah675@gmail.com>
2024-09-05 23:46:58 +0000

    added Google provider and error redirect to signin page

--- image-ai/src/auth.ts
+++ image-ai/src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github"; // Use GitHubProvider instead of just GitHub
+import Google from "next-auth/providers/google"; // Use GitHubProvider instead of just GitHub
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 
 import { db } from '@/db/drizzle';
@@ -7,10 +8,12 @@ import { db } from '@/db/drizzle';
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
   providers: [
-    GitHub
+    GitHub,
+    Google,
   ],
   pages: {
     signIn: '/sign-in',
+    error: '/sign-in',
   }
 
 
