fe12b6e9b81411f5693ef778e2f6f612abc6d396
Yaw Amo Mensah <amomensah300@gmail.com>
2024-09-05 16:35:41 +0100

    added page.tsx for sign up

---
+++ image-ai/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,19 @@
+import { redirect } from 'next/navigation';
+
+import { auth } from '@/auth';
+import { SignUpCard } from '@/features/auth/components/sign-up';
+
+
+const SignUpPage = async () => {
+    const session = await auth();
+
+    if (session) {
+        redirect('/');
+    }
+
+    return (
+            <SignUpCard />
+    )
+};
+
+export default SignUpPage; 
