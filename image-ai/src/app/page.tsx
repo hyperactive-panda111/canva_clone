15355db8257197cdd8adc278cc5f7372eab6572d
amomensah675 <amomensah675@gmail.com>
2024-09-06 00:08:56 +0000

    added changes

--- image-ai/src/app/page.tsx
+++ image-ai/src/app/page.tsx
@@ -2,11 +2,13 @@ import { auth } from '@/auth';
 import { protectServer } from '@/features/auth/utils';
 
 export default async function Home() {
-  protectServer();
+  await protectServer();
+
+  const session = auth()
   
   return (
     <div>
-     You are logged in
+     {JSON.stringify(session)}
     </div>
   );
 }
