a28add001bb2e373cabbac43c59499feff5c0fc0
amomensah675 <amomensah675@gmail.com>
2024-09-07 02:58:11 +0000

    added files

--- image-ai/src/app/(auth)/sign-up/page.tsx
+++ image-ai/src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
 import { auth } from '@/auth';
-import { SignUpCard } from '@/features/auth/components/sign-up';
+import { SignUpCard } from '@/features/auth/components/sign-up-card';
 
 
 const SignUpPage = async () => {
