e37eb70667a80a8e8647970ca82099100ee7a8fa
amomensah675 <amomensah675@gmail.com>
2024-09-09 02:38:31 +0000

    added changes

--- image-ai/src/app/(dashboard)/page.tsx
+++ image-ai/src/app/(dashboard)/page.tsx
@@ -1,14 +1,13 @@
-import { auth } from '@/auth';
 import { protectServer } from '@/features/auth/utils';
+
+import { Banner } from './banner';
 
 export default async function Home() {
   await protectServer();
-
-  const session = await auth();
   
   return (
-    <div>
-     {JSON.stringify(session)}
+    <div className='flex flex-col space-y-6 max-w-screen-xl mx-auto pb-10'>
+      <Banner />
     </div>
   );
 }
