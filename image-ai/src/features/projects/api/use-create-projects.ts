3d8c3e85b96532f0558816b3c48c55e2a87c9906
Yaw Amo Mensah <amomensah300@gmail.com>
2024-09-10 09:53:21 +0100

    added usecreateproject hook

---
+++ image-ai/src/features/projects/api/use-create-projects.ts
@@ -0,0 +1,35 @@
+import { useMutation } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+
+import { client } from "@/lib/hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.projects["$post"], 200>;
+type RequestType = InferRequestType<typeof client.api.projects["$post"]>["json"];
+
+export const useCreateProject = () => {
+  const mutation = useMutation<
+    ResponseType,
+    Error,
+    RequestType
+  >({
+    mutationFn: async (json) => {
+      const response = await client.api.projects.$post({ json });
+
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success('Project created');
+      //TODO: Invalidate 'projects' query
+    },
+    onError: () => {
+      toast.error('Failed to create project');
+    },
+  });
+
+  return mutation;
+};
