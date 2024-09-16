7303120b893b0b775fe97f105eb81db5c3308378
Yaw Amo Mensah <amomensah300@gmail.com>
2024-09-17 00:25:33 +0100

    added load-state hook

---
+++ image-ai/src/features/editor/hooks/use-load-state.ts
@@ -0,0 +1,44 @@
+import { fabric } from 'fabric';
+import { useRef, useEffect } from 'react';
+
+import { JSON_KEYS } from '@/features/editor/types';
+
+interface UseLoadStateProps {
+    autoZoom: () => void;
+    canvas: fabric.Canvas | null;
+    initialState: React.MutableRefObject<string | undefined>;
+    canvasHistory: React.MutableRefObject<string[]>;
+    setHistoryIndex: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const useLoadState = ({
+    autoZoom,
+    canvas,
+    initialState,
+    canvasHistory,
+    setHistoryIndex
+}: UseLoadStateProps) => {
+    const initialized = useRef(false);
+
+    useEffect(() => {
+        if (!initialized.current && initialState?.current && canvas) {
+            const data = JSON.parse(initialState.current);
+            canvas.loadFromJSON(data, () => {
+                const currentState = JSON.stringify(
+                    canvas.toJSON(JSON_KEYS),
+                );
+
+                canvasHistory.current = [currentState];
+                setHistoryIndex(0);
+                autoZoom();
+            });
+            initialized.current = true;
+        }
+    }, [
+        canvas,
+        autoZoom,
+        initialState,
+        canvasHistory,
+        setHistoryIndex
+    ]);
+}
