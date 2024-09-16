6272cc507eaf195bb6cfd9c0ad0d7e880ccf5353
Yaw Amo Mensah <amomensah300@gmail.com>
2024-09-17 00:15:29 +0100

    added patch method for editing existing project

--- image-ai/src/app/api/[[...route]]/projects.ts
+++ image-ai/src/app/api/[[...route]]/projects.ts
@@ -3,10 +3,87 @@ import { projects, projectsInsertSchema } from '@/db/schema';
 
 import { verifyAuth } from '@hono/auth-js';
 import { zValidator } from '@hono/zod-validator';
+import { eq, and } from 'drizzle-orm';
 import { Hono } from 'hono';
 import { z } from 'zod';
 
 const app = new Hono()
+    .patch(
+        '/:id',
+        verifyAuth(),
+        zValidator('param', z.object({ id: z.string() })),
+        zValidator(
+            'param',
+            z.object({ id: z.string() }),
+       ),
+       zValidator(
+              'json',
+              projectsInsertSchema.omit({
+                    createdAt: true,
+                    updatedAt: true,
+                    id: true,
+                    userId: true, 
+              }).partial(),
+        ),
+        async (c) => {
+            const auth = c.get('authUser');
+            const { id } = c.req.valid('param');
+            const values = c.req.valid('json');
+
+            if (!auth.token?.id) {
+                return c.json({ error: 'Unauthorized'}, 401);
+            }
+
+            const data = await db
+                .update(projects)
+                .set({
+                    ...values,
+                    updatedAt: new Date(),
+                })
+                .where(
+                    and(
+                        eq(projects.id, id),
+                        eq(projects.userId, auth.token.id)
+                    )
+                ).returning();
+
+            if (data.length === 0) {
+                return c.json({ error: 'Unauthorized'}, 401);
+            }
+
+            return c.json({ data: data[0] });
+        }
+    )
+    .get(
+        '/:id',
+        verifyAuth(),
+        zValidator('param', z.object({ id: z.string() })),
+        async (c) => {
+            const auth = c.get('authUser');
+            const { id } = c.req.valid('param');
+
+            if (!auth.token?.id) {
+                return c.json({ error: 'Unauthorized'}, 401);
+
+            }
+
+            const data = await db
+                .select()
+                .from(projects)
+                .where(
+                    and(
+                      eq(projects.id, id),
+                      eq(projects.userId, auth.token.id)
+                    )
+                );
+
+            if (data?.length === 0) {
+                return c.json({ error: 'Not found'}, 404);
+            }
+
+            return c.json({ data: data[0]});
+        }
+    )
     .post(
         '/',
         verifyAuth(),
