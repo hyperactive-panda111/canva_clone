9b32f40dbffb862ca0a83d56bafc830b41db7a4b
Yaw Amo Mensah <amomensah300@gmail.com>
2024-09-17 00:18:48 +0100

    added changes

--- image-ai/src/app/editor/[projectId]/page.tsx
+++ image-ai/src/app/editor/[projectId]/page.tsx
@@ -1,14 +1,50 @@
-import { protectServer } from '@/features/auth/utils';
+'use client';
+
+import { useGetProject } from '@/features/projects/api/use-get-project';
 import { Editor } from '@/features/editor/components/editor'
+import { Loader, TriangleAlert } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
-const EditorProjectIdPage = async () => {
-    await protectServer();
+interface EditorProjectIdPageProps {
+    params: {
+        projectId: string;
+    }
+}
 
-    return (
-        <div>
-            <Editor />
-        </div>
-    );
+const EditorProjectIdPage = ({
+    params,
+}: EditorProjectIdPageProps) => {  
+    const { data, isError, isLoading, } = useGetProject(params.projectId);
+    
+    if (!data || isLoading) {
+        return (
+            <div className='h-full flex flex-col items-center justify-center'>
+                <Loader className='size-6 animate-spin text-muted-foreground'/>
+            </div>
+        )
+    }
+
+    if (isError) {
+        return (
+            <div className='h-full flex flex-col gap-y-5 items-center justify-center'>
+                <TriangleAlert className='size-6 text-muted-foreground' />
+                <p className='text-sm text-muted-foreground '>
+                Failed to fetch project
+                </p>
+                <Button 
+                    asChild
+                    variant='secondary'
+                >
+                    <Link href={'/'}>
+                        Back to Home
+                    </Link>
+                </Button>
+            </div>
+        )
+    }
+
+    return  <Editor initialData={data}/> ;
 
 }
 
