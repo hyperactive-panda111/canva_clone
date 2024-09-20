35715a94ef5687e3b2b082fbed2bcc98d5609fa1
Yaw Amo Mensah <amomensah300@gmail.com>
2024-09-20 07:17:33 +0100

    added delete projects route

--- image-ai/src/app/api/[[...route]]/projects.ts
+++ image-ai/src/app/api/[[...route]]/projects.ts
@@ -8,6 +8,34 @@ import { Hono } from 'hono';
 import { z } from 'zod';
 
 const app = new Hono()
+    .delete(
+        '/:id',
+        verifyAuth(),
+        zValidator('param', z.object({ id: z.string() })),
+        async (c) => {
+            const auth = c.get('authUser');
+            const { id } = c.req.valid('param');
+
+            if (!auth.token?.id) {
+                return c.json({ error: 'Unauthorized'}, 401);
+            }
+
+            const data = await db
+                .delete(projects)
+                .where(
+                    and(
+                        eq(projects.id, id),
+                        eq(projects.userId, auth.token.id),
+                    )
+                ).returning()
+
+                if (data.length === 0) {
+                    return c.json({ error: 'Not Found'}, 401);
+                }
+
+                return c.json({ data: { id} });
+        }
+    )
     .post(
         '/:id/duplicate',
         verifyAuth(),
