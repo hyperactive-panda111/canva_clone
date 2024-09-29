edc60164ccdee158b728c5048b06f390366e7a73
Yaw Amo Mensah <amomensah300@gmail.com>
2024-09-29 02:48:50 +0100

    added use-checkout.ts

---
+++ image-ai/src/features/subscriptions/api/use-checkout.ts
@@ -0,0 +1,33 @@
+import { useMutation } from "@tanstack/react-query";
+import { InferResponseType } from "hono";
+
+import { client } from "@/lib/hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<typeof client.api.subscriptions.checkout['$post'], 200>;
+
+export const useCheckout = () => {
+  
+  const mutation = useMutation<
+    ResponseType,
+    Error
+  >({
+    mutationFn: async (json) => {
+      const response = await client.api.subscriptions.checkout.$post({ json });
+
+      if (!response.ok) {
+        throw new Error('Failed to create session');
+      }
+
+      return await response.json();
+    },
+    onSuccess: ({ data}) => {
+      window.location.href = data;
+    },
+    onError: () => {
+      toast.error('Failed to create session');
+    },
+  });
+
+  return mutation;
+};
