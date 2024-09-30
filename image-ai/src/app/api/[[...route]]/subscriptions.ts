6823b1b330c4f8102315ccff005429f6b5a91579
Yaw Amo Mensah <amomensah300@gmail.com>
2024-09-30 03:21:39 +0100

    added billing route

--- image-ai/src/app/api/[[...route]]/subscriptions.ts
+++ image-ai/src/app/api/[[...route]]/subscriptions.ts
@@ -1,8 +1,61 @@
 import { verifyAuth } from '@hono/auth-js';
 import { Hono } from 'hono';
 import { stripe } from '@/lib/stripe';
+import Stripe from 'stripe';
+import { db } from '@/db/drizzle';
+import { subscriptions } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+import { checkIsActive } from '@/features/subscriptions/lib';
 
 const app = new Hono()
+    .post('/billing', verifyAuth(), async (c) => {
+        const auth = c.get('authUser');
+
+        if (!auth.token?.id) {
+            return c.json({ error : 'Unauthorized'}, 401);
+        }
+
+        const [subscription] = await db
+            .select()
+            .from(subscriptions)
+            .where(eq(subscriptions.id, auth.token.id));
+
+        if (!subscription) {
+            return c.json({ error: 'No subscription found'}, 404);
+        }
+
+        const session = await stripe.billingPortal.sessions.create({
+            customer: subscription.customerId,
+            return_url: `${process.env.NEXT_PUBLIC_APP_URL}`,
+        });
+
+        if (!session.url) {
+            return c.json({ error: 'Failed to create session'}, 400);
+        }
+
+        return c.json({ data: session.url });
+    })
+    .get('/current', verifyAuth(), async (c) => {
+        const auth = c.get('authUser');
+
+        if (!auth.token?.id) {
+            return c.json({ error : 'Unauthorized'}, 401);
+        }
+
+        const [subscription] = await db
+            .select()
+            .from(subscriptions)
+            .where(eq(subscriptions.id, auth.token.id));
+
+        const active = checkIsActive(subscription);
+
+        return c.json({ 
+            data: {
+                ...subscription,
+                active,
+            }
+        });
+    })
     .post('/checkout',
         verifyAuth(),
         async (c) => {
@@ -37,6 +90,72 @@ const app = new Hono()
             }
 
             return c.json({ data: url })
+        })
+        .post('/webhook', async (c) => {
+            const body = await c.req.text();
+            const signature = c.req.header('Stripe-Signature') as string;
+
+            let event: Stripe.Event;
+
+            try {
+                event = stripe.webhooks.constructEvent(
+                    body,
+                    signature,
+                    process.env.STRIPE_WEBHOOK_SECRET!,
+                )
+            } catch (error) {
+                return c.json({ error: 'Invalid signature' }, 400);
+            }
+
+            const session = event.data.object as Stripe.Checkout.Session;
+
+            if (event.type === 'checkout.session.completed') {
+                const subscription = await stripe.subscriptions.retrieve(
+                    session.subscription as string,
+                );
+
+                if (!session?.metadata?.userId) {
+                    return c.json({ error: 'Invalid session' }, 400);
+                }
+
+                await db
+                    .insert(subscriptions)
+                    .values({
+                        status: subscription.status,
+                        userId: session.metadata.userId,
+                        subscriptionId: subscription.id,
+                        customerId: subscription.customer as string,
+                        priceId: subscription.items.data[0].price.product as string,
+                        currentPeriodEnd: new Date(
+                            subscription.current_period_end * 1000
+                        ),
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                    });
+            }
+
+            if (event.type === 'invoice.payment_succeeded') {
+                const subscription = await stripe.subscriptions.retrieve(
+                    session.subscription as string,
+                );
+
+                if (!session?.metadata?.userId) {
+                    return c.json({ error: 'Invalid session' }, 400);
+                }
+
+                await db
+                    .update(subscriptions)
+                    .set({
+                        status: subscription.status,
+                        currentPeriodEnd: new Date(subscription.current_period_end * 1000),
+                        updatedAt: new Date(),
+                    }).where(
+                        eq(subscriptions.id, subscription.id)
+                    )
+            }
+            return c.json(null, 200);
+
+
         });
 
 export default app;
